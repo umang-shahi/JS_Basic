fdf4a887659047b3495c9a9da98d18c733ff6545
umang <umangshahi800@gmail.com>
2023-01-05 16:19:34 +0545

    Learning Basic JS

---
+++ index.js
@@ -0,0 +1,37 @@
+// var x=3;
+// console.log(x);
+// console.log(x*x*x);
+// x=12;
+// console.log(x);
+// let a=12;
+// console.log(a);
+// const pie=3.14;
+// console.log(pie);
+// alert("Oye kukur!! Bolna");
+ 
+// const x="8";
+// x=int(x);
+// console.log(x);
+// console.log(typeof(x));
+
+// let bool = true;
+// console.log(typeof(bool));
+
+// let x = 90
+// let y = 67
+// let z = false
+// let result = x+y+z
+// console.log(result)
+
+// let name = "JavaScript";
+// let y = 90;
+// let find = name+ y;
+// console.log(find);
+
+const firstname = "Arya"
+const surname = "Doctor"
+console.log(firstname +" "+ surname)
+console.log("My name is : " + firstname + surname)
+console.log("My name is : " + firstname+ " " + surname)
+const Doctor = (`My name is : ${firstname} ${surname}.`)
+console.log(`Hello Nurse ❤️ ${Doctor}`)
