6ced5c596d77b600fc1a69da6981b49fd1166288
umang <umangshahi800@gmail.com>
2023-01-08 16:23:15 +0545

    conditions

--- index.js
+++ index.js
@@ -36,68 +36,210 @@
 // const Doctor = (`My name is : ${firstname} ${surname}.`)
 // console.log(`Hello Nurse ❤️ ${Doctor}`)
 
-let name = "adrian Pradhan";
-console.log(name)
-console.log(typeof name)
-name = Number(name)
-console.log(typeof name)
+// let name = "adrian Pradhan";
+// console.log(name)
+// console.log(typeof name)
+// name = Number(name)
+// console.log(typeof name)
 
-// Type conversion and String methods
+// // Type conversion and String methods
 
-// let number = 112;
-// console.log(number)
-// console.log(typeof number)
-// console.log(typeof String(number))
+// // let number = 112;
+// // console.log(number)
+// // console.log(typeof number)
+// // console.log(typeof String(number))
 
-// Strings Methods
+// // Strings Methods
 
-let str = "Hello Nurse"
-let y = "I am a doctor"
-//CharAt
-console.log(str.charAt(7))
-//Concatenation
-console.log(str.concat(y))
-//length
-console.log(str.length)
-//Replace
-console.log(str.replace('Nurse','Baby'))
-//Uppercase
-console.log(str.toUpperCase())
-//Lowercase
-console.log(str.toLowerCase())
-//Split
-console.log(str.split(""))
-//Split while there is a space
-console.log(str.split(" "))
-//Knowing the index
-console.log(str.indexOf("s"))
-//Last index of method
-console.log(y.lastIndexOf("o"))
-//Includes
-console.log(str.includes("Nurse"))
-let a = "     Ladki ho toh Nurse     "
-console.log(a.trim())
-//Slicing
-console.log(str.slice(0,7)) //It doesnot include the exact last data of the index
-
-
-
-const heading = document.getElementById('mycss');
-heading.innerText = "hello umang";
-
-
-heading.style.background="red";
-heading.style.color = "white";
-heading.style.textAlign ="center";
+// let str = "Hello Nurse"
+// let y = "I am a doctor"
+// //CharAt
+// console.log(str.charAt(7))
+// //Concatenation
+// console.log(str.concat(y))
+// //length
+// console.log(str.length)
+// //Replace
+// console.log(str.replace('Nurse','Baby'))
+// //Uppercase
+// console.log(str.toUpperCase())
+// //Lowercase
+// console.log(str.toLowerCase())
+// //Split
+// console.log(str.split(""))
+// //Split while there is a space
+// console.log(str.split(" "))
+// //Knowing the index
+// console.log(str.indexOf("s"))
+// //Last index of method
+// console.log(y.lastIndexOf("o"))
+// //Includes
+// console.log(str.includes("Nurse"))
+// let a = "     Ladki ho toh Nurse     "
+// console.log(a.trim())
+// //Slicing
+// console.log(str.slice(0,7)) //It doesnot include the exact last data of the index
 
 
 
-let x=10;
-if(x<5) {
-    console.log("x is greater than 5");
-}else {
-    console.log("x is greater or equal to 5 ");
-}
+// const heading = document.getElementById('mycss');
+// heading.innerText = "hello umang";
+
+
+// heading.style.background="red";
+// heading.style.color = "white";
+// heading.style.textAlign ="center";
+
+
+
+
+
+//conditions
+
+// let email = "umangshahi800gmail.com";
+// let pass = "1111111111";
+// if (email===umangshahi800gmail.com && pass==="1111111111"){
+    // console.log("login success!");
+// } else {
+    // console.log("invalid!");
+// } 
+
+
+
+//x=10
+
+// let x=10;
+
+
+// if (x > 15) {
+//     console.log("x is gretater than 15")
+// } else if (x>5) {
+//     console.log("x is greater than 15 but not greater than 5")
+// }else {
+//     console.log("x is not greater than 15")
+// }
+
+//weather
+
+// let weather ="winter";
+
+// if (weather==="sunny"){
+//     console.log("beautiful day");
+// }else if(weather==="cloudy") {
+//     console.log(" a bit cloudy");
+// }else if (weather==="rainy"){
+//     console.log("rainy day");
+// }else if(weather==="winter "){
+//     console.log("jaddooo");
+// }else {
+//     console.log("invalid");
+// }
+
+
+//switch
+
+// let day= "sunday";
+// switch (day)  {
+//     case "sunday":
+//         console.log("today is sunday");
+//         break;
+
+//         case "monday":
+//         console.log("today is monday");
+//         break;
+
+//         case "tuesday":
+//         console.log("today is tuesday");
+//         break;
+
+//         case "wednesday":
+//         console.log("today is wednesday");
+//         break;
+
+//         case "thrusday":
+//         console.log("today is thrusday");
+//         break;
+
+//         case "friday":
+//         console.log("today is friday");
+//         break;
+
+//         case "saturday":
+//         console.log("today is saturday");
+//         break;
+
+//     default:
+//         console.log("invalid day");
+//         break;
+// }
+
+// let grade="A";
+
+// switch (grade) {
+//         case "A":
+//         console.log("excellent ");
+//         break;
+
+//         case "B":
+//             console.log("very good");
+//             break;
+
+//             case  "C":
+//                 console.log("good ");
+//                 break;
+
+//                 case "D":
+//                     console.log("bad");
+//                     break;
+
+//                     case "E":
+//                         console.log("fail");
+//                         break;
+
+//     default:
+//         console.log(" grade not found ");
+//         break;
+// }
+
+
+// let month="feb";
+
+// switch (month) {
+//     case "june":
+//         case "july":
+//         console.log("q1");
+//         break;
+
+//         case "janaury":
+//         case "august":
+
+//             console.log("q2");
+//             break;
+
+//             case "feb":
+//             case "october":
+
+//                 console.log("q3");
+//                 break;
+
+//                 case "november":
+//                 case "december":
+
+//                     console.log("q4");
+//                     break;
+
+//     default:
+//         console.log("not in the queue");
+//         break;
+// }
+
+
+
+
+
+
+
+
 
 
 
